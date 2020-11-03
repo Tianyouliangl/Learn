6eb413ecf7f8a27b2048ec4e8ae40e773fe88c84
fzw <fengzhangwei399@yeah.net>
2020-11-03 17:10:50 +0800

    commit

---
+++ slidingMenuLibrary/build/generated/source/buildConfig/debug/com/jeremyfeinstein/slidingmenu/lib/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.jeremyfeinstein.slidingmenu.lib;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.jeremyfeinstein.slidingmenu.lib";
+  public static final String BUILD_TYPE = "debug";
+}
